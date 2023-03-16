4c402914557cc0174373ff94981696b581226624
Kathryn Panger <kpanger88@gmail.com>
2023-03-16 18:55:16 -0500

    splicer object in progress

--- splicer.py
+++ splicer.py
@@ -1,5 +1,17 @@
-
-# This class takes in a dictionary of file names and the pages from each file you would like to splice together
+import PyPDF2
+# This class takes in a dictionary of file names and the pages from each file you would like to splice together.
+# List them in the order you would like the final pdf to have them in. This may mean adding the same file
+# to the list twice with different pages specified, if you want to splice something in the middle, for example
 class splicer():
     def __init__(self, filesToPages:dict[str:list[int]]):
         self.filesToPages = filesToPages
+
+    def makePdf(self):
+        extractedPages = []
+        for fileName, pageNumbers in self.filesToPages.items():
+            pdfFileObj = open(f'{fileName}', 'rb')
+            pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+            for page in pageNumbers:
+                pageObj = pdfReader.getPage(page)
+                extractedPages.append(pageObj)
+
