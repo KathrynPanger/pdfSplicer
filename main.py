752b552ad170de7448b8cf58bb0f1562e78426f6
Kathryn Panger <kpanger88@gmail.com>
2023-03-16 19:23:10 -0500

    drop files in pdfs folder

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from splicer import Splicer
 zipDict = {"pdfs/bluebonnet_agreement.pdf": [1,2,3,4], "pdfs/lastPage.pdf": [1]}
-splicer = Splicer(zipDict, "signed_agreement.pdf")
+splicer = Splicer(zipDict, "pdfs/signed_agreement.pdf")
 splicer.makePdf()
--- splicer.py
+++ splicer.py
@@ -1,15 +1,18 @@
 import PyPDF2
+
 # This class takes in a dictionary of file names and the pages from each file you would like to splice together.
 # List them in the order you would like the final pdf to have them in. This may mean adding the same file
 # to the list twice with different pages specified, if you want to splice something in the middle, for example
+
 #NOTE: Use actual page numbers, not page indicies
+
 class Splicer():
     def __init__(self, filesToPages, newFileName):
         self.filesToPages = {}
         for fileName, pageNumbers in filesToPages.items():
             self.filesToPages[fileName] = [item - 1 for item in pageNumbers]
-
         self.newFileName = newFileName
+
     def makePdf(self):
         pdfWriter = PyPDF2.PdfWriter()
         for fileName, pageNumbers in self.filesToPages.items():
