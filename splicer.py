9759f8f7169006b347a526b60ce134e7365c5995
Kathryn Panger <kpanger88@gmail.com>
2023-03-16 19:03:57 -0500

    gitignore

--- splicer.py
+++ splicer.py
@@ -3,15 +3,18 @@ import PyPDF2
 # List them in the order you would like the final pdf to have them in. This may mean adding the same file
 # to the list twice with different pages specified, if you want to splice something in the middle, for example
 class splicer():
-    def __init__(self, filesToPages:dict[str:list[int]]):
+    def __init__(self, filesToPages:dict[str:list[int]], newFileName):
         self.filesToPages = filesToPages
-
+        self.newFileName = newFileName
     def makePdf(self):
-        extractedPages = []
+        pdfWriter = PyPDF2.PdfFileWriter()
         for fileName, pageNumbers in self.filesToPages.items():
             pdfFileObj = open(f'{fileName}', 'rb')
             pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
             for page in pageNumbers:
                 pageObj = pdfReader.getPage(page)
-                extractedPages.append(pageObj)
+                pdfWriter.addPage(pageObj)
 
+        newFile = open(newFileName, 'wb')
+        pdfWriter.write(newFileName)
+        newFile.close()
