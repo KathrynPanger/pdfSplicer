f911016ade835ce0e0701b74b094ff41a892f527
Kathryn Panger <kpanger88@gmail.com>
2023-03-16 19:19:28 -0500

    finished pdf comination class

--- splicer.py
+++ splicer.py
@@ -2,19 +2,23 @@ import PyPDF2
 # This class takes in a dictionary of file names and the pages from each file you would like to splice together.
 # List them in the order you would like the final pdf to have them in. This may mean adding the same file
 # to the list twice with different pages specified, if you want to splice something in the middle, for example
-class splicer():
-    def __init__(self, filesToPages:dict[str:list[int]], newFileName):
-        self.filesToPages = filesToPages
+#NOTE: Use actual page numbers, not page indicies
+class Splicer():
+    def __init__(self, filesToPages, newFileName):
+        self.filesToPages = {}
+        for fileName, pageNumbers in filesToPages.items():
+            self.filesToPages[fileName] = [item - 1 for item in pageNumbers]
+
         self.newFileName = newFileName
     def makePdf(self):
-        pdfWriter = PyPDF2.PdfFileWriter()
+        pdfWriter = PyPDF2.PdfWriter()
         for fileName, pageNumbers in self.filesToPages.items():
             pdfFileObj = open(f'{fileName}', 'rb')
-            pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+            pdfReader = PyPDF2.PdfReader(pdfFileObj)
             for page in pageNumbers:
-                pageObj = pdfReader.getPage(page)
-                pdfWriter.addPage(pageObj)
+                pageObj = pdfReader.pages[page]
+                pdfWriter.add_page(pageObj)
 
-        newFile = open(newFileName, 'wb')
-        pdfWriter.write(newFileName)
+        newFile = open(self.newFileName, 'wb')
+        pdfWriter.write(newFile)
         newFile.close()
